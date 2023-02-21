2a5e707c38c1795667d688fe0aafb20631acf530
elichka <elichka.cs@outlook.com>
2023-02-21 23:34:13 +0100

    Fix toString method but still have work to do

--- src/models/core/GameBoard.java
+++ src/models/core/GameBoard.java
@@ -18,7 +18,6 @@ public class GameBoard {
 
     public GameBoard() {
         this.barn = new Barn();
-        System.out.println(this.barn);
         this.leftGarden = new Garden(new Coordinates(-1, 0));
         this.rightGarden = new Garden(new Coordinates(1, 0));
         this.field = new Field(new Coordinates(0, 1));
@@ -27,6 +26,14 @@ public class GameBoard {
         board.put(this.leftGarden.getCoordinates(), this.leftGarden);
         board.put(this.rightGarden.getCoordinates(), this.rightGarden);
         board.put(this.field.getCoordinates(), this.field);
+        board.put(new Coordinates(-2, 0), this.field);
+        board.put(new Coordinates(-3, 0), this.field);
+        board.put(new Coordinates(-3, 1), this.field);
+        board.put(new Coordinates(-3, 1), this.field);
+        board.put(new Coordinates(-3, 2), this.field);
+        board.put(new Coordinates(-3, 3), this.field);
+        board.put(new Coordinates(-3, 4), this.field);
+        board.put(new Coordinates(-4, 0), this.field);
         this.tileMatrixForPrinting = this.getTileMatrix();
         System.out.println(this);
     }
@@ -44,16 +51,21 @@ public class GameBoard {
     }
 
     private String[][] getTileMatrix() {
-        int maxWidth = this.board.lastKey().x() - this.board.firstKey().x();
+        List<Integer> xCoordinatesSorted = this.board.keySet().stream().map(Coordinates::x).sorted().toList();
+        int maxWidth = xCoordinatesSorted.get(xCoordinatesSorted.size() - 1) - xCoordinatesSorted.get(0);
         List<Integer> yCoordinatesSorted = this.board.keySet().stream().map(Coordinates::y).sorted().toList();
         int maxHeight = yCoordinatesSorted.get(yCoordinatesSorted.size() - 1) - yCoordinatesSorted.get(0);
         String[][] matrix = new String[maxHeight + 1][maxWidth + 1];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                Tile tile = this.board.getOrDefault(new Coordinates(j - maxHeight, maxWidth - i - 1), null);
+                int x = j - maxWidth + 1;
+                int y = maxHeight - i;
+                Tile tile = this.board.getOrDefault(new Coordinates(x, y), null);
                 if (tile == null) {
-                    char[] emptyRow = new char[6];
+                    char[] emptyRow = new char[7];
                     Arrays.fill(emptyRow, ' ');
+                    emptyRow[0] = '#';
+                    emptyRow[emptyRow.length - 1] = '#';
                     matrix[i][j] = (String.valueOf(emptyRow) + System.lineSeparator() +
                             String.valueOf(emptyRow) + System.lineSeparator() + String.valueOf(emptyRow));
                 } else {
@@ -80,9 +92,15 @@ public class GameBoard {
             firstRowBuilder.append(System.lineSeparator());
             secondRowBuilder.append(System.lineSeparator());
             thirdRowBuilder.append(System.lineSeparator());
-            gameBoardBuilder.append(firstRowBuilder.toString().replaceAll("\\|\\|", "|"))
-                    .append(secondRowBuilder.toString().replaceAll("\\|\\|", "|"))
-                    .append(thirdRowBuilder.toString().replaceAll("\\|\\|", "|"));
+            gameBoardBuilder.append(firstRowBuilder.toString()
+                            .replaceAll("\\|\\|", "|")
+                            .replaceAll("##|#", " "))
+                    .append(secondRowBuilder.toString()
+                            .replaceAll("\\|\\|", "|")
+                            .replaceAll("##|#", " "))
+                    .append(thirdRowBuilder.toString()
+                            .replaceAll("\\|\\|", "|")
+                            .replaceAll("##|#", " "));
         }
 
         return gameBoardBuilder.toString();
